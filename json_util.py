d820c30be8b4adc557be1468b401f79153e41ea9
Ryan <rynkrn321@gmail.com>
2020-02-09 20:50:23 -0500

    added object_hook in JSONDecoder which decodes ISO strings into datetime objects

--- json_util.py
+++ json_util.py
@@ -28,7 +28,61 @@ class MongoJSONEncoder(JSONEncoder):
 
 
 class JSONDecoder(json.JSONDecoder):
-    pass
+    def __init__(self, *, object_hook=None, parse_float=None, parse_int=None, 
+        parse_constant=None, strict=True, object_pairs_hook=None):
+        
+        if object_hook is None and hasattr(self, 'object_hook'):
+            object_hook = self.object_hook 
+        
+        if parse_float is None and hasattr(self, 'parse_float'):
+            parse_float = self.parse_float 
+
+        if parse_int is None and hasattr(self, 'parse_int'):
+            parse_int = self.parse_int 
+
+        if parse_constant is None and hasattr(self, 'parse_constant'):
+            parse_constant = self.parse_constant 
+
+        if object_pairs_hook is None and hasattr(self, 'object_pairs_hook'):
+            object_pairs_hook = self.object_pairs_hook
+
+        super().__init__(object_hook=object_hook, parse_float=parse_float, parse_int=parse_int, 
+            parse_constant=parse_constant, strict=strict, object_pairs_hook=object_pairs_hook)
+
+    def object_hook(self, obj):
+        if type(obj) == dict:
+            iterator = obj.items()
+        elif type(obj) == list:
+            iterator = enumerate(obj)
+
+        for k, v in iterator:
+            if type(v) in (dict, list):
+                obj[k] = self.object_hook(v)
+            if type(v) == str:
+                obj[k] = self.parse_str(v) 
+
+        return obj
+
+    def parse_str(self, s):
+        try:
+            if s.count('-') == 2:
+                return datetime.fromisoformat(s)
+            if s.count(':') == 2:
+                return time.fromisoformat(s)
+            #add logic for UUIDs
+        except ValueError:
+            pass
+        return s
+
+
+class MongoJSONDecoder(JSONDecoder):
+    def object_hook(self, obj):
+        if '_id' in obj:
+            try:
+                obj['_id'] = ObjectId(obj['_id'])
+            except InvalidId:
+                pass
+        return super().object_hook(obj)
 
 
 def to_decimal(s):
@@ -37,33 +91,3 @@ def to_decimal(s):
 
 def to_decimal128(s):
     return Decimal128(s)
-
-
-def iso_object_hook(obj):
-    for k, v in obj.items():
-        if type(v) == dict:
-            return obj[k] = iso_object_hook(v)
-        if type(v) == list:
-            pass
-        if type(v) == str:
-            try:
-                if v.count('-') == 2:
-                    obj[k] = datetime.fromisoformat(v)
-                elif v.count(':') == 2:
-                    obj[k] = time.fromisoformat(v)
-            except ValueError:
-                pass
-    return obj
-
-
-def mongo_object_hook(obj):
-    """
-        An object_hook function for decoding JSON
-        Converts the '_id' key into an instance of ObjectId 
-    """
-    if '_id' in obj:
-        try:
-            obj['_id'] = ObjectId(obj['_id'])
-        except InvalidId:
-            pass
-    return obj
