173f529a11e2a4d432332a448d0d0bf83948deca
Ryan <rynkrn321@gmail.com>
2020-02-13 08:13:45 -0500

    may add a descriptor for the hash_name of a RedisModel

--- descriptors.py
+++ descriptors.py
@@ -4,3 +4,11 @@ class CollectionDescriptor:
         if instance is not None:
             raise AttributeError("Collection isn't accessible via {} instances".format(owner.__name__))
         return owner.database[owner.__name__.lower()]
+
+
+class HashNameDescriptor:
+    def __get__(self, instance, owner):
+        pass
+
+    def __set__(self):
+        pass
