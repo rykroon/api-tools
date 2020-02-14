9d12b8ae6bda49a8767ec24e6f4fa9d372c7b28a
Ryan <rynkrn321@gmail.com>
2020-02-13 22:14:59 -0500

    added HashNameDescriptor

--- descriptors.py
+++ descriptors.py
@@ -5,10 +5,18 @@ class CollectionDescriptor:
             raise AttributeError("Collection isn't accessible via {} instances".format(owner.__name__))
         return owner.database[owner.__name__.lower()]
 
+    def __set__(self, instance, value):
+        pass
+
 
 class HashNameDescriptor:
-    def __get__(self, instance, owner):
-        pass
+    def __init__(self):
+        self.name = None
 
-    def __set__(self):
-        pass
+    def __get__(self, instance, owner):
+        if self.name is None:
+            return owner.__name__.lower()
+        return self.name
+
+    def __set__(self, instance, value):
+        self.name = value
